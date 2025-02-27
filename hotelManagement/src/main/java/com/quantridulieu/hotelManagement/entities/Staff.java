ddc39d0b8255a6a307c1199794f4ac20e35a5954
“PhungQuocThai” <phungquocthai.aeron.dev@gmail.com>
2025-02-27 19:13:42 +0700

    completed basic config

--- hotelManagement/src/main/java/com/quantridulieu/hotelManagement/entities/Staff.java
+++ hotelManagement/src/main/java/com/quantridulieu/hotelManagement/entities/Staff.java
@@ -1,16 +1,12 @@
 package com.quantridulieu.hotelManagement.entities;
 
-import lombok.*;
 import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
 @Table(name = "Staff")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class Staff {
-    // Content remains the same, just updated import
     @Id
     @Column(name = "staff_id")
     private String staffId;
@@ -26,5 +22,56 @@ public class Staff {
     
     @Column(name = "password")
     private String password;
+    
+    public Staff() {}
+
+	public Staff(String staffId, String staffName, String staffPhone, Date dateOfBirth, String password) {
+		super();
+		this.staffId = staffId;
+		this.staffName = staffName;
+		this.staffPhone = staffPhone;
+		this.dateOfBirth = dateOfBirth;
+		this.password = password;
+	}
+
+	public String getStaffId() {
+		return staffId;
+	}
+
+	public void setStaffId(String staffId) {
+		this.staffId = staffId;
+	}
+
+	public String getStaffName() {
+		return staffName;
+	}
+
+	public void setStaffName(String staffName) {
+		this.staffName = staffName;
+	}
+
+	public String getStaffPhone() {
+		return staffPhone;
+	}
+
+	public void setStaffPhone(String staffPhone) {
+		this.staffPhone = staffPhone;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
    
 }
