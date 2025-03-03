76a1a6c3bc973ffc1ea5746f78cee29185a785d3
ngtrhui <trihuy2004@gmail.com>
2025-03-03 21:15:23 +0700

    addadd

--- hotelManagement/src/main/java/com/quantridulieu/hotelManagement/repositories/InvoiceRepository.java
+++ hotelManagement/src/main/java/com/quantridulieu/hotelManagement/repositories/InvoiceRepository.java
@@ -3,14 +3,21 @@ package com.quantridulieu.hotelManagement.repositories;
 import java.util.List;
 import java.util.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.quantridulieu.hotelManagement.entities.Invoice;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-@Repository
+
 public interface InvoiceRepository extends JpaRepository<Invoice, String> {
-	List<Invoice> findByInvoiceId(String invoiceId);
-    List<Invoice> findByInvoiceDate(Date invoiceDate);
-    List<Invoice> findByTotalAmount(float totalAmount);
-    List<Invoice> findByUsId(String usId);
-    List<Invoice> findByStaffId(String staffId);
+	  @Query(value = "SELECT * FROM invoice WHERE invoice_date = :invoiceDate", nativeQuery = true)
+	    List<Invoice> findByInvoiceDate(@Param("invoiceDate") Date invoiceDate);
+
+	    @Query(value = "SELECT * FROM invoice WHERE total_amount = :totalAmount", nativeQuery = true)
+	    List<Invoice> findByTotalAmount(@Param("totalAmount") float totalAmount);
+
+	    @Query(value = "SELECT * FROM invoice WHERE us_id = :usId", nativeQuery = true)
+	    List<Invoice> findByUsId(@Param("usId") String usId);
+
+	    @Query(value = "SELECT * FROM invoice WHERE staff_id = :staffId", nativeQuery = true)
+	    List<Invoice> findByStaffId(@Param("staffId") String staffId);
 }
