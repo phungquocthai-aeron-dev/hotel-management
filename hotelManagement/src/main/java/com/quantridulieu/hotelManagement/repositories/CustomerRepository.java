fb028452842e3d16b0beedba06c741991b29c6d9
“PhungQuocThai” <phungquocthai.aeron.dev@gmail.com>
2025-03-03 17:24:48 +0700

    completed basic entities and export excel service

--- hotelManagement/src/main/java/com/quantridulieu/hotelManagement/repositories/CustomerRepository.java
+++ hotelManagement/src/main/java/com/quantridulieu/hotelManagement/repositories/CustomerRepository.java
@@ -1,8 +1,21 @@
 package com.quantridulieu.hotelManagement.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.quantridulieu.hotelManagement.entities.Customer;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, String> {}
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+	@Query(value = "SELECT * FROM customer WHERE customer_name = :customerName", nativeQuery = true)
+	List<Customer> findCustomerByName(@Param("customerName") String customerName);
+	
+	@Query(value = "SELECT * FROM customer WHERE customer_phone = :customerPhone", nativeQuery = true)
+	Customer findCustomerByPhone(@Param("customerPhone") String customerPhone);
+	
+	@Query(value = "SELECT * FROM customer WHERE address = :address", nativeQuery = true)
+	List<Customer> findCustomerByAddress(@Param("address") String address);
+}
