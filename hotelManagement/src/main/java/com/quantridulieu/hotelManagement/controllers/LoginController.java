92e1afcd75c96d0edfff8bbb0711249987800f68
ngtrhui <trihuy2004@gmail.com>
2025-03-08 19:52:34 +0700

    loginlogin

--- hotelManagement/src/main/java/com/quantridulieu/hotelManagement/controllers/LoginController.java
+++ hotelManagement/src/main/java/com/quantridulieu/hotelManagement/controllers/LoginController.java
@@ -1,25 +1,36 @@
 package com.quantridulieu.hotelManagement.controllers;
 
+
+
 import com.quantridulieu.hotelManagement.entities.Staff;
 import com.quantridulieu.hotelManagement.services.StaffService;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/") // Đảm bảo Spring nhận diện controller
 public class LoginController {
 
     @Autowired
     private StaffService staffService;
     
     @PostMapping("/login")
-    public String login(@RequestParam String phone, @RequestParam String password, Model model) {
+    public String login(@RequestParam String phone, @RequestParam String password, Model model, HttpSession session) {
         Staff staff = staffService.getStaffByPhone(phone);
-        if (staff != null && staff.getPassword().equals(password)) {
-            // Đăng nhập thành công, chuyển đến trang home
+        if (staff != null && password.equals(staff.getPassword())) { 
+        	 // Lưu thông tin vào session
+            session.setAttribute("loggedInStaff", staff);
             return "redirect:/home";
         } else {
             // Đăng nhập thất bại, trả về trang login với thông báo lỗi
@@ -30,6 +41,11 @@ public class LoginController {
     @GetMapping("/login")
     public String showLoginPage() {
         return "login"; // Chỉ ra tên của JSP hoặc view bạn muốn trả về
+    }
+    
+    @Bean
+    PasswordEncoder passwordEncoder() {
+    	return new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2Y, 10);
     }
 
 }
--- hotelManagement/src/main/java/com/quantridulieu/hotelManagement/repositories/StaffRepository.java
+++ hotelManagement/src/main/java/com/quantridulieu/hotelManagement/repositories/StaffRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import com.quantridulieu.hotelManagement.entities.Staff;
 
 @Repository
@@ -15,7 +16,7 @@ public interface StaffRepository extends JpaRepository<Staff, String> {
 	List<Staff> findStaffByStaffName(@Param("staffName") String staffName);
 	
 	@Query(value = "SELECT * FROM staff WHERE staff_phone = :staff_phone", nativeQuery = true)
-	Staff findStaffByPhone(@Param("staffPhone") String staffPhone);
+	Staff findStaffByPhone(@Param("staff_phone") String staffPhone);
 	
 	@Query(value = "SELECT * FROM staff WHERE dob = :dob", nativeQuery = true)
 	List<Staff> findStaffByDOB(@Param("dob") Date dob);
