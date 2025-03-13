d579fcde73f7953536ee7ea1a8c733852d80be12
“PhungQuocThai” <phungquocthai.aeron.dev@gmail.com>
2025-03-13 20:14:02 +0700

    resolved conflict

--- hotelManagement/src/main/java/com/quantridulieu/hotelManagement/entities/Room.java
+++ hotelManagement/src/main/java/com/quantridulieu/hotelManagement/entities/Room.java
@@ -1,70 +1,65 @@
-package com.quantridulieu.hotelManagement.entities;
-
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "Room")
-public class Room {
-    @Id
-    @Column(name = "room_id")
-    private String roomId;
-    
-    @Column(name = "room_number")
-    private Integer roomNumber;
-    
-    @Column(name = "status")
-    private String status;
-
-<<<<<<< HEAD
-    @ManyToOne
-    @JoinColumn(name = "category_id", insertable = false, updatable = false)
-=======
-    @ManyToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "category_id")
-    @OnDelete(action = OnDeleteAction.SET_NULL)
->>>>>>> origin/PhungQuocThai_V4
-    private Category category;
-    
-    public Room() {}
-
-	public Room(String roomId, Integer roomNumber, String status, Category category) {
-		super();
-		this.roomId = roomId;
-		this.roomNumber = roomNumber;
-		this.status = status;
-		this.category = category;
-	}
-
-	public String getRoomId() {
-		return roomId;
-	}
-
-	public void setRoomId(String roomId) {
-		this.roomId = roomId;
-	}
-
-	public Integer getRoomNumber() {
-		return roomNumber;
-	}
-
-	public void setRoomNumber(Integer roomNumber) {
-		this.roomNumber = roomNumber;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public Category getCategory() {
-		return category;
-	}
-
-	public void setCategory(Category category) {
-		this.category = category;
-	}
-    
-}
+//package com.quantridulieu.hotelManagement.entities;
+//
+//import jakarta.persistence.*;
+//
+//@Entity
+//@Table(name = "Room")
+//public class Room {
+//    @Id
+//    @Column(name = "room_id")
+//    private String roomId;
+//    
+//    @Column(name = "room_number")
+//    private Integer roomNumber;
+//    
+//    @Column(name = "status")
+//    private String status;
+//
+//    @ManyToOne(cascade = CascadeType.PERSIST)
+//    @JoinColumn(name = "category_id")
+//    @OnDelete(action = OnDeleteAction.SET_NULL)
+//    private Category category;
+//    
+//    public Room() {}
+//
+//	public Room(String roomId, Integer roomNumber, String status, Category category) {
+//		super();
+//		this.roomId = roomId;
+//		this.roomNumber = roomNumber;
+//		this.status = status;
+//		this.category = category;
+//	}
+//
+//	public String getRoomId() {
+//		return roomId;
+//	}
+//
+//	public void setRoomId(String roomId) {
+//		this.roomId = roomId;
+//	}
+//
+//	public Integer getRoomNumber() {
+//		return roomNumber;
+//	}
+//
+//	public void setRoomNumber(Integer roomNumber) {
+//		this.roomNumber = roomNumber;
+//	}
+//
+//	public String getStatus() {
+//		return status;
+//	}
+//
+//	public void setStatus(String status) {
+//		this.status = status;
+//	}
+//
+//	public Category getCategory() {
+//		return category;
+//	}
+//
+//	public void setCategory(Category category) {
+//		this.category = category;
+//	}
+//    
+//}
